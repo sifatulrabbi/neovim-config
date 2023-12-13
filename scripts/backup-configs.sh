93d260c76d3c147ae29ec00e7ba3accbf68e3c3c
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2023-12-13 18:51:52 +0600

    helpeful scripts

---
+++ scripts/backup-configs.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+mv ~/.local/share/nvim ~/.local/share/nvim.bak
+mv ~/.config/nvim ~/.config/nvim.bak
---
+++ scripts/use-backup-configs.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+mv ~/.local/share/nvim.bak ~/.local/share/nvim
+mv ~/.config/nvim.bak ~/.config/nvim
